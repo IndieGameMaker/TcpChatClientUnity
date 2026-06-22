4b4705422d51c7ee44b5c0e1a96653a1e285349d
Lee JaeHyun <myleje@gmail.com>
2026-06-22 10:13:07 +0900

    Implement TcpChatClient class with IDisposable support and connection management

--- Assets/Scripts/TcpChatClient.cs
+++ Assets/Scripts/TcpChatClient.cs
@@ -1,6 +1,27 @@
-using UnityEngine;
+using System;
+using System.IO;
+using System.Net.Sockets;
 
-public class TcpChatClient
+public class TcpChatClient : IDisposable
 {
+    private string _serverIp;
+    private int _serverPort;
     
+    // TCP 관련한 변수 선언
+    private TcpClient _tcpClient;
+    private NetworkStream _stream;
+    private StreamReader _reader;
+    private StreamWriter _writer;
+    
+    // 서버 상태 변수
+    private bool _isConnected;
+    public bool IsConnected => _isConnected;
+    
+    // 릴리즈 여부 변수
+    private bool _isDisposed;
+    
+    public void Dispose()
+    {
+        // TODO: 릴리즈 작업
+    }
 }
